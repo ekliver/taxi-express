c11b9bfa679e1ec44fd175d8737ea6cca25f58f9
ekliver <jekliver@gmail.com>
2015-10-03 04:57:07 +0000

    Primera subida

---
+++ web/app/controllers/articles_controller.rb
@@ -0,0 +1,66 @@
+class ArticlesController < ApplicationController
+#before_action :validate_user, except: [:show,:index]
+before_action :authenticate_user!, only: [:create,:new]
+before_action :set_article, except: [:index,:new,:create]
+    #GET /articles
+    def index
+        #@articles variable gloval
+        @articles= Article.all
+    end
+    #GET /articles/:id
+    def show
+        @article.update_visits_count
+        
+    end
+    #GET /articles/new
+    def new
+        @article=Article.new
+    end
+    def edit
+        
+    end
+    #POST /articles
+    def create
+        #INSERT INTO
+        @article = current_user.articles.new(article_params)
+        if @article.save
+            redirect_to @article
+        else
+            render :new
+        end
+    end
+    
+    
+    #DELETE FROM articles
+    def destroy
+       
+        @article.destroy #Destroy eliminar el objeto de la base de datos
+        redirect_to articles_path
+    end
+    #PUT /article/:id
+    def update
+        #UPDATE
+        #@article.update_atributes({title: "Nuevo titulo"})
+        
+        if @article.update(article_params)
+        redirect_to @article
+        else
+            render :edit
+        end
+        
+    end
+    
+    private
+    
+    def set_article
+        @article=Article.find(params[:id]) 
+    end
+    def validate_user
+       redirect_to new_user_session_path,notice: "Necesitas iniciar sesión" 
+    end
+    def article_params
+    params.require(:article).permit(:title,:body)
+    end
+
+
+end
---
+++ web/app/models/article.rb
@@ -0,0 +1,21 @@
+class Article < ActiveRecord::Base
+    belongs_to :user
+    #la tabla=> articles
+    #campos => Article.text
+    #Escribir metodos
+    validates :title, presence: true, uniqueness: true
+    validates :body, presence: true, length: {minimum: 20}
+    before_save :set_visits_count
+    
+    
+    def update_visits_count
+        #self.save if self.visits_count.nil?
+        self.update(visits_count: self.visits_count+1)
+    end
+    
+    private
+    def set_visits_count
+    self.visits_count ||=0
+    end
+    
+end
